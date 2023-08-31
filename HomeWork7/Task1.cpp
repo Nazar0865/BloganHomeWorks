9d1d0797989f078b4a46a7ade2876b9773ccf7cf
Nazar0865 <65090001+Nazar0865@users.noreply.github.com>
2023-08-31 16:43:04 +0300

    Add files via upload
    Home Work number 7 added

---
+++ HomeWork7/Task1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+int main()
+{
+	/*1. Вивести на екран наступний текст:
+      I'm trying to do my best.
+      Help me with it.
+   Замітьте, що треба вивести кожне речення в окремому рядку.*/
+
+	std::cout << "I'm trying to do my best." << std::endl;
+	std::cout << "Help me with it." << std::endl;
+	return 0;
+}
---
+++ HomeWork7/Task2.cpp
@@ -0,0 +1,8 @@
+﻿/*2. Виведіть на екран любі 4 речення. Кожний рядок має мати 2 речення.*/
+#include <iostream>
+int main()
+{
+	std::cout << "It's just a text." << " Batman the start is a bad film." << std::endl;
+	std::cout << "I want to be programmer. " << "The syntax of C++ very nice." << std::endl;
+	return 0;
+}
